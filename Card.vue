fd23a775e7a534288ad056a4e99d81714a0d283f
vivek <vivek@athenaeum.in>
2021-05-03 18:20:00 +0530

    done

--- Card.vue
+++ Card.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h4>Card Component</h4>
+    <p>First Name: {{ firstName }}</p>
+    <p>Last Name: {{ lastName }}</p>
+    <p>Year: {{ year }}</p>
+    <p>Active? {{ isActive ? "Yes" : "No" }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["firstName", "lastName", "year", "isActive"],
+};
 </script>
 
 <style>
