339980345f46187ed2520c24213990e1cd55c963
vivek <vivek@athenaeum.in>
2021-05-03 18:16:33 +0530

    done

---
+++ Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h4>Card Component</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Container Component</h3>
+    <hr />
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from "./Card.vue";
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style>
+</style>
